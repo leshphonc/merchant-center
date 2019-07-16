fb7d72607b865df8eac7e98b1705c5ba1b1948d4
cc <102075776@qq.com>
2019-07-16 11:23:27 +0800

    list

--- src/pages/modules/settings/basicinformation/index.tsx
+++ src/pages/modules/settings/basicinformation/index.tsx
@@ -5,18 +5,17 @@ import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack'
+import ChevronRightIcon from '@material-ui/icons/ChevronRight'
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import ListSubheader from '@material-ui/core/ListSubheader'
 import List from '@material-ui/core/List'
 import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
 import ListItemText from '@material-ui/core/ListItemText'
 import Divider from '@material-ui/core/Divider'
-import InboxIcon from '@material-ui/icons/Inbox'
 import { BasicInformation } from '@/config/list'
 
 const useStyles = makeStyles({
   root: {
-    background: '#fff',
     position: 'absolute',
     top: 0,
     bottom: 0,
@@ -31,15 +30,24 @@ const useStyles = makeStyles({
     width: 100,
     margin: '0 auto',
   },
+  listItem: {
+    background: '#fff',
+  },
 })
 export default (props: any) => {
   const classes = useStyles()
   const mapList = () => BasicInformation.map(item => (
     <List key={item.label} subheader={<ListSubheader>{item.label}</ListSubheader>}>
+      <Divider />
       {item.children.map(child => (
         <div key={child.label}>
-          <ListItem button>
+          <ListItem button className={classes.listItem}>
             <ListItemText primary={child.label} />
+            <ListItemSecondaryAction>
+              <IconButton>
+                <ChevronRightIcon />
+              </IconButton>
+            </ListItemSecondaryAction>
           </ListItem>
           <Divider />
         </div>
