57ce0ae924091ed2d323e804409d545208385ffd
cc <102075776@qq.com>
2019-07-17 19:16:03 +0800

    发红包

--- src/pages/modules/promotion/redEnvelope/index.tsx
+++ src/pages/modules/promotion/redEnvelope/index.tsx
@@ -1,28 +1,11 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import NavBar from '@/components/NavBar'
-import { RedEnvelopeListData } from '@/config/list'
-import Fab from '@material-ui/core/Fab'
-import AddIcon from '@material-ui/icons/Add'
-import RedEnvelopeList from './components/RedEnvelopeList'
+import { Route } from 'react-router-dom'
+import RedEnvelopeList from './list'
+import RedEnvelopePublish from './publish'
 
-const useStyles = makeStyles({
-  fab: {
-    position: 'fixed',
-    bottom: 50,
-    right: 30,
-    color: '#fff',
-  },
-})
-export default () => {
-  const classes = useStyles()
-  return (
-    <React.Fragment>
-      <NavBar title="红包列表" goBack rightBtn={{ label: '发布红包', url: 'url' }} />
-      <RedEnvelopeList list={RedEnvelopeListData} />
-      {/* <Fab color="primary" aria-label="Add" className={classes.fab}>
-        <AddIcon />
-      </Fab> */}
-    </React.Fragment>
-  )
-}
+export default () => (
+  <React.Fragment>
+    <Route path="/promotion/redEnvelope" exact component={RedEnvelopeList} />
+    <Route path="/promotion/redEnvelope/publish" component={RedEnvelopePublish} />
+  </React.Fragment>
+)
---
+++ src/pages/modules/promotion/redEnvelope/publish.tsx
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default () => <div>发红包</div>
